34a40a6b526eb03a101dfd90d5a6ca250d17043b
Jinwu Yang <131998405+jinwuyang@users.noreply.github.com>
2024-12-22 03:36:11 +0800

    a little change

--- hipans/utils/PtxUtils.h
+++ hipans/utils/PtxUtils.h
@@ -13,132 +13,78 @@ namespace hipans {
 
 __device__ __forceinline__ unsigned int
 getBitfield(uint8_t val, int pos, int len) {
-  unsigned int ret;
-  int num=(1<<len)-1;
-  ret=(val>>(pos-1))&num;
-  return ret;
+  return (val>>(pos-1))&((1<<len)-1);
 }
 
 __device__ __forceinline__ unsigned int
 getBitfield(uint16_t val, int pos, int len) {
-  unsigned int ret;
-  int num=(1<<len)-1;
-  ret=(val>>(pos-1))&num;
-  return ret;
+  return (val>>(pos-1))&((1<<len)-1);
 }
 
 __device__ __forceinline__ unsigned int
 getBitfield(unsigned int val, int pos, int len) {
-  unsigned int ret;
-  int num=(1<<len)-1;
-  ret=(val>>(pos-1))&num;
-  return ret;
+  return (val>>(pos-1))&((1<<len)-1);
 }
 
 __device__ __forceinline__ uint64_t
 getBitfield(uint64_t val, int pos, int len) {
-  uint64_t ret;
-  int num=(1<<len)-1;
-  ret=(val>>(pos-1))&num;
-  return ret;
+  return (val>>(pos-1))&((1<<len)-1);
 }
 
 __device__ __forceinline__ unsigned int
 setBitfield(unsigned int val, unsigned int toInsert, int pos, int len) {
-  unsigned int ret;
-  unsigned int temp = ((1<<len)-1)&toInsert;
-  temp = temp << pos;//400
-  ret = ((1<<len)-1)<<pos;
-  ret = ((~ret) & val) | temp;
-  return ret;
+  return ((~(((1<<len)-1)<<pos)) & val) | ((((1<<len)-1)&toInsert) << pos);
 }
 
 __device__ __forceinline__ uint32_t rotateLeft(uint32_t v, uint32_t shift) {
-  uint32_t out;
   uint32_t n = min(shift, 32);
-  out = (v << n) | (v >> (32 - n));
-  return out;
+  return (v << n) | (v >> (32 - n));
 }
 
 __device__ __forceinline__ uint32_t rotateRight(uint32_t v, uint32_t shift) {
-  uint32_t out;
   uint32_t n = min(shift, 32);
-  out = (v << (32 - n)) | (v >> n);
-  return out;
+  return (v << (32 - n)) | (v >> n);
 }
-
 __device__ __forceinline__ int getLaneId() {
-  int laneId;
-  laneId = threadIdx.x % kWarpSize;
-  return laneId;
+  return threadIdx.x & 63;
 }
-
 __device__ __forceinline__ uint64_t getLaneMaskLt() {
-  uint64_t mask;
-  int laneId;
-  laneId = threadIdx.x % kWarpSize;
-  mask = (1ULL << laneId) - 1;
-  return mask;
+  return (1ULL << getLaneId()) - 1;
 }
 
 __device__ __forceinline__ uint64_t getLaneMaskLe() {
-  uint64_t mask;
-  int laneId;
-  laneId = threadIdx.x % kWarpSize;
-  if(laneId == 63)
-  mask = 0xffffffffffffffff;
-  mask =(1ULL << (laneId+1)) - 1;
-  return mask;
+  return (1ULL << (getLaneId() + 1)) - 1;
 }
 
 __device__ __forceinline__ uint64_t getLaneMaskGt() {
-  uint64_t mask;
-  int laneId;
-  laneId = threadIdx.x % kWarpSize;
-  if(laneId == 63)
-  mask = 0xffffffffffffffff;
-  else
-  mask = (1ULL << (laneId+1)) - 1;
-  mask = ~mask;
-  return mask;
+  return (~((1ULL << (getLaneId() + 1)) - 1)) & 0xFFFFFFFFFFFFFFFF;
 }
 
 __device__ __forceinline__ uint64_t getLaneMaskGe() {
-  uint64_t mask;
-  int laneId;
-  laneId = threadIdx.x % kWarpSize;
-  mask = (1ULL << laneId) - 1;
-  mask = ~mask;
-  return mask;
+  return ~((1ULL << getLaneId()) - 1);
 }
 
 template <typename T>
 __device__ inline T warpReduceAllMin(T val) {
 #pragma unroll
-  for (int mask = kWarpSize / 2; mask > 0; mask >>= 1) {
+  for (int mask = kWarpSize / 2; mask > 0; mask >>= 1) 
     val = min(val, __shfl_xor(val, mask, kWarpSize));
-  }
-
   return val;
 }
 
 template <typename T, int Width = kWarpSize>
 __device__ inline T warpReduceAllMax(T val) {
 #pragma unroll
-  for (int mask = Width / 2; mask > 0; mask >>= 1) {
+  for (int mask = Width / 2; mask > 0; mask >>= 1)
     val = max(val, __shfl_xor(val, mask, kWarpSize));
-  }
-
   return val;
 }
 
 template <typename T, int Width = kWarpSize>
 __device__ inline T warpReduceAllSum(T val) {
 #pragma unroll
-  for (int mask = Width / 2; mask > 0; mask >>= 1) {
+  for (int mask = Width / 2; mask > 0; mask >>= 1) 
     val += __shfl_xor(val, mask, kWarpSize);
-  }
-
   return val;
 }
 
